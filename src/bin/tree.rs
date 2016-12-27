5e2dadd5d8c5e514d5c73728c11a13e45a4e9964
Leandro Ostera <leandro@ostera.io>
2016-12-28 00:12:39 +0100

    Stuff

--- src/bin/tree.rs
+++ src/bin/tree.rs
@@ -1,12 +1,20 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{App};
+use clap::{App, Arg};
 
 fn main() {
     let matches = App::new("tree")
         .author(crate_authors!())
         .version(crate_version!())
         .about("A GNU Tree-like app built in Rust")
+        .arg(Arg::with_name("level")
+             .short("l")
+             .long("level")
+             .value_name("LEVEL")
+             .takes_value(true)
+             .help("How many levels of depth to go when printing the tree"))
+        .arg(Arg::with_name("path")
+             .help("Path to start from"))
         .get_matches();
 }
